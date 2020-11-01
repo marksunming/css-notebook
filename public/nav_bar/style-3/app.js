0604d2a8d9a58105c4b6e84aecc817b102eb17b5
SunMing <sunming@SunMings-MacBook-Air.local>
2020-11-01 11:03:43 -0700

    style-3 nav bar animation finish

--- public/nav_bar/style-3/app.js
+++ public/nav_bar/style-3/app.js
@@ -1,21 +1,24 @@
 const sections = document.querySelectorAll('section');
 const bubble = document.querySelector('.bubble');
 const gradients = [
-    "background: linear-gradient(to right top, #f46b45, #eea849)",
-    "background: linear-gradient(to right top, #005c97, #363795)",
-    "background: linear-gradient(to right top, #e53935, #e35d5b)"
+    "linear-gradient(to right top, #f46b45, #eea849)",
+    "linear-gradient(to right top, #005c97, #363795)",
+    "linear-gradient(to right top, #e53935, #e35d5b)"
 ];
 
 const options = {
     threshold: 0.7
-}
+};
+
+let currentAnchor;
+let activeAnchor;
 
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries){
     entries.forEach(entry => {
         const className = entry.target.className;
-        const activeAnchor = document.querySelector(`[data-page=${className}]`);
+        activeAnchor = document.querySelector(`[data-page=${className}]`);
         const gradientIndex = entry.target.getAttribute('data-index');
         const coords = activeAnchor.getBoundingClientRect();
         const directions = {
@@ -24,16 +27,44 @@ function navCheck(entries){
             top: coords.top,
             left: coords.left
         };
+        // console.log(activeAnchor);
+        // console.log(currentAnchor);
+        // console.log(entry.isIntersecting)
         if(entry.isIntersecting){
             bubble.style.setProperty('left',`${directions.left}px`);
             bubble.style.setProperty('top',`${directions.top}px`);
             bubble.style.setProperty('width',`${directions.width}px`);
             bubble.style.setProperty('height',`${directions.height}px`);
             bubble.style.background = gradients[gradientIndex];
+            activeAnchor.style.color = "white";
+            if(currentAnchor){
+                currentAnchor.style.color = "black";
+            }
+            
+        }else {
+            currentAnchor = document.querySelector(`[data-page=${className}]`);
         }
+        
     })
 }
 
 sections.forEach(section => {
     observer.observe(section)
 });
+
+const body = document.querySelector("body");
+// console.log(body)
+window.addEventListener("resize", () => {
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+        height: coords.height,
+        width: coords.width,
+        top: coords.top,
+        left: coords.left
+    };
+    bubble.style.setProperty('left',`${directions.left}px`);
+    bubble.style.setProperty('top',`${directions.top}px`);
+    bubble.style.setProperty('width',`${directions.width}px`);
+    bubble.style.setProperty('height',`${directions.height}px`);
+    console.log(activeAnchor)
+})
